fc8f7ab1598fb8286496f9df1fa54cf5662888b5
Jason Zhang <jasonzhang@Jasons-MacBook-Air.local>
2022-01-16 13:29:41 -0500

    landing page has been styled

--- src/components/Vision.js
+++ src/components/Vision.js
@@ -1,15 +1,42 @@
 import "./Vision.css"
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
 
-export default function vision() {
+export default function Vision() {
     return (
         <>
             <div className="welcome-page">
                 <h3>
                     Welcome to NFTracker
                 </h3>
-                <br />
-                To get started, log into your account or sign up for one for free today!
+                <p style={ {fontStyle: "italic", color: "grey"} }>
+                    Powered By the OpenSea API
+                </p>
+
+                <div className="content">
+                    <Carousel className="carousel" autoPlay="true" interval="7000" infiniteLoop="true" showThumbs={false} width="35%">
+                        <div>
+                            <img src="https://lh3.googleusercontent.com/IDwnhttw3NRmKl3-n0EBjxaqW44v3zniDXeuJ81T6YEsQ2cNgEJbdZKNpRV2vPAgT7u05AQhF98UN6fH1gNn7hQUeAwSu4KiYtSdgg=w600" alt="nft-1" />
+                            {/* <p>PHANTA BEAR</p> */}
+                        </div>
+                        <div>
+                            <img src="https://lh3.googleusercontent.com/bta6Z9f3nTllFDHDfKRe6oe99JJ0hlF09Mt_-twvs_Rmazzf0mkiSEJi-x1iyqDeE8-AQ0eYlqlMoGy-4_WdR6UbtqvH4FvMRGUA=w600" alt="nft-2" />
+                            {/* <p>DOODLES</p> */}
+                        </div>
+                        <div>
+                            <img src="https://lh3.googleusercontent.com/HSfcLMGJG2onm4SP3pmjsHZpVrKd89SME9PCwOLxbEH_kcHg1eisIZMFCHcreylzEj277nxuvCSlTF7hRfy8cqdW_wRiuqDgJNBJEq8=w600" alt="nft-3" />
+                            {/* <p>CRYPTOPUNKS</p> */}
+                        </div>
+                    </Carousel>
+
+                    <p>Are you having a hard time following the prices of all the NFTs you want?</p>
+                    <p>Look no further because NFTracker allows you to store all your wanted NFTs into one place!</p>
+                    <p>Sign up for an account today and start tracking!</p>
+                </div>
             </div>
         </>
     )
 }
+
+// ReactDOM.render(<Vision />, document.querySelector('.demo-carousel'));
